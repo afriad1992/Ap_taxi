46861e97a951357961bb005dc79877abaccb4ddb
afriad1992 <56979262+afriad1992@users.noreply.github.com>
2020-06-10 16:20:51 +0200

    Create taxis.controller.js

---
+++ controllers/taxis.controller.js
@@ -0,0 +1,42 @@
+const { createTaxi } = require('../queries/taxis.queries');
+const multer = require('multer');
+const path = require('path');
+
+
+const upload = multer({ storage: multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join( __dirname, '../public/images/avatars'))
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${ Date.now() }-${ file.originalname }`);
+  }
+}) })
+
+exports.signupFormT = (req, res, next) => {
+  res.render('taxis/taxi-form', { errors: null, isAuthenticated :req.isAuthenticated(),currentTaxi:req.taxi,chauffeur:true });
+ 
+}
+
+exports.signupT = async (req, res, next) => {
+  const body = req.body;
+  try {
+    const taxi = await createTaxi(body);
+    res.redirect('/');
+  } catch(e) {
+    res.render('taxis/taxi-form', { errors: [ e.message ],isAuthenticated :req.isAuthenticated(),currentTaxi:req.taxi });
+  }
+}
+
+exports.uploadImageT = [ 
+  upload.single('avatar'),
+  async (req, res, next) => {
+    try {
+      const taxi = req.taxi;
+      taxi.avatar = `/images/avatars/${req.file.filename}`;
+      await taxi.save();
+      res.redirect('/');
+    } catch(e) {
+      next(e);
+    }
+  }
+]
