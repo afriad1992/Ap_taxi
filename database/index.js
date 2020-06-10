a1a8ce0fd626482422fab4f3f5e6771e543220df
afriad1992 <56979262+afriad1992@users.noreply.github.com>
2020-06-10 16:21:20 +0200

    Create index.js

---
+++ database/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const env = require(`../environment/${ process.env.NODE_ENV }`);
+
+mongoose.connect(env.dbUrl, { 
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then( () => console.log('connexion db ok !')).catch( err => console.log(err));
