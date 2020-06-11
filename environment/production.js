8486edcf04963f868a8746fd0efd2f334d5ade3c
afriad1992 <56979262+afriad1992@users.noreply.github.com>
2020-06-11 12:07:48 +0200

    Revert "Update production.js"
    This reverts commit 672d71b2bf9ffbacaeb45b0cd793759165f74ad2.

--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   dbUrl: 'mongodb+srv://tchatter:tchats@cluster0-khmxt.gcp.mongodb.net/tchatter?retryWrites=true&w=majority',
-  cert: '/etc/letsencrypt/live/www.jdtaxi.xyz/fullchain.pem',
-  key: '/etc/letsencrypt/live/www.jdtaxi.xyz/privkey.pem'
+  cert: '',
+  key: ''
 }
